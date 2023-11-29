95012a0bbf144ab1dcc87726d72ad768106d0850
0xBA55 <101381775+0xBA55@users.noreply.github.com>
2023-11-29 20:25:11 +0100

    Update DDLGrupo8.sql

--- Proyecto/DDLGrupo8.sql
+++ Proyecto/DDLGrupo8.sql
@@ -3,13 +3,14 @@
 --------------------------------------------------------------------------|
 
 -- Crear la tabla SOLICITANTE
+DROP TABLE SOLICITANTE CASCADE CONSTRAINTS;
 CREATE TABLE SOLICITANTE (
-    nombre_solicitante VARCHAR(20) PRIMARY KEY,
-    nombre VARCHAR2(50),
+    nombre_solicitante VARCHAR(50) PRIMARY KEY,
     direccion VARCHAR2(100)
 );
 
 -- Crear la tabla CONTACTO
+DROP TABLE CONTACTO CASCADE CONSTRAINTS;
 CREATE TABLE CONTACTO (
     tlf NUMBER PRIMARY KEY,
     cargo VARCHAR2(20),
@@ -17,10 +18,13 @@ CREATE TABLE CONTACTO (
     nombre_contacto VARCHAR2(50),
     fax NUMBER,
     FOREIGN KEY (nombre_fabricante) REFERENCES FABRICANTE(nombre_fabricante),
-    FOREIGN KEY (nombre_solicitante) REFERENCES SOLICITANTE(nombre_solicitante)
+    FOREIGN KEY (nombre_solicitante) REFERENCES SOLICITANTE(nombre_solicitante),
+    CONSTRAINT check_digitos_tlf CHECK (tlf > 0 AND LENGTH(TO_CHAR(tlf)) = 9),
+    CONSTRAINT check_digitos_fax CHECK (fax > 0 AND LENGTH(TO_CHAR(tlf)) = 10)
 );
 
 -- Crear la tabla COMPONENTE
+DROP TABLE COMPONENTE CASCADE CONSTRAINTS;
 CREATE TABLE COMPONENTE (
     nombre_comun VARCHAR2(50) PRIMARY KEY,
     tipo VARCHAR2(50),
@@ -29,25 +33,29 @@ CREATE TABLE COMPONENTE (
     numeros VARCHAR2(20),
     formulas VARCHAR2(50),
     peso_molecular NUMBER,
-    contenido VARCHAR2(50)
+    contenido VARCHAR2(50),
+    CONSTRAINT check_peso_positivo CHECK (peso_molecular > 0)
 );
 
 -- Crear la tabla FABRICANTE
+DROP TABLE FABRICANTE CASCADE CONSTRAINTS;
 CREATE TABLE FABRICANTE (
     nombre_fabricante VARCHAR(50) PRIMARY KEY,
     direccion VARCHAR2(100)
 );
 
 -- Crear la tabla FÁBRICA
+DROP TABLE FABRICA CASCADE CONSTRAINTS;
 CREATE TABLE FABRICA (
     nombre_fabrica VARCHAR(50) PRIMARY KEY,
     direccion VARCHAR2(100),
     metodo_fabricacion VARCHAR2(100),
-    nombre_fabricante NUMBER,
+    nombre_fabricante VARCHAR(50),
     FOREIGN KEY (nombre_fabricante) REFERENCES FABRICANTE(nombre_fabricante)
 );
 
 -- Crear la tabla SUSTANCIA_ACTIVA
+DROP TABLE SUSTANCIA_ACTIVA CASCADE CONSTRAINTS;
 CREATE TABLE SUSTANCIA_ACTIVA (
     cod_desarrollo NUMBER PRIMARY KEY,
     nombre_comun VARCHAR2(50),
@@ -86,7 +94,23 @@ CREATE TABLE SUSTANCIA_ACTIVA (
     tension_superficial VARCHAR2(20),
     propiedades_comburentes VARCHAR2(100),
     informacion_2.9 VARCHAR2(100),
-    informacion_2.10 VARCHAR2(100)
+    informacion_2.10 VARCHAR2(100),
+    CONSTRAINT check_cod_desarrollo CHECK (cod_desarrollo > 0 AND LENGTH(TO_CHAR(cod_desarrollo)) = 7),
+    CONSTRAINT check_fecha_registro CHECK (fecha_registro >= TO_DATE('2010-01-01', 'YYYY-MM-DD')),
+);
+
+
+
+------------------------------------------------------------------------|
+---------------------------COMUN----------------------------------------|
+------------------------------------------------------------------------|
+
+
+
+-- Crear la tabla SOLICITANTE
+CREATE TABLE SOLICITANTE (
+    nombre_solicitante NUMBER PRIMARY KEY,
+    direccion VARCHAR2(100)
 );
 
 
@@ -109,13 +133,6 @@ CREATE TABLE MICROORGANISMO (
     metodos_criterios VARCHAR2(200),
     codigos VARCHAR2(50),
     relaciones_patogenos VARCHAR2(200)
-);
-
--- Crear la tabla SOLICITANTE
-CREATE TABLE SOLICITANTE (
-    id_solicitante NUMBER PRIMARY KEY,
-    nombre VARCHAR2(50),
-    direccion VARCHAR2(100)
 );
 
 -- Crear la tabla CONTACTO
